e018bfbd41231ac8225dd95c977083f445368beb
Harshita Kaushik <h.kaushik1029@gmail.com>
2025-07-23 16:08:23 +0530

    Add files via upload

---
+++ src/components/utils/ServiceCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function ServiceCard({ image, tag, title, location }) {
+  return (
+    <div className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow">
+    <div className="w-full h-fit overflow-hidden " >
+      <img 
+        src={image} 
+        alt={title} 
+        className="w-full h-[320px] object-contain flex item-center justify-center pl-4 pt-4"
+      />
+    </div>
+    <div className="p-4">
+ 
+      <h3 className="text-lg font-semibold text-gray-900 mb-1">{title}</h3>
+      <p className="text-sm text-gray-600">{location}</p>
+    </div>
+  </div>
+  );
+}
+
+export default ServiceCard;
